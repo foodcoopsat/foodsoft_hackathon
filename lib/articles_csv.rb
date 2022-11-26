f2e6b76e4262a8c29104987a76b688d4e5a44f9a
Florian Lentsch <office@florian-lentsch.at>
2022-11-26 18:47:28 +0100

    Proposal for #13

--- lib/articles_csv.rb
+++ lib/articles_csv.rb
@@ -13,30 +13,42 @@ class ArticlesCsv < RenderCSV
       Article.human_attribute_name(:price),
       Article.human_attribute_name(:tax),
       Article.human_attribute_name(:deposit),
-      Article.human_attribute_name(:unit_quantity),
+      Article.human_attribute_name(:quantity),
+      Article.human_attribute_name(:supplier_order_unit),
+      Article.human_attribute_name(:price_unit),
+      Article.human_attribute_name(:group_order_unit),
+      Article.human_attribute_name(:group_order_granularity),
+      Article.human_attribute_name(:minimum_order_quantity),
       '',
       '',
       Article.human_attribute_name(:article_category),
+      Article.human_attribute_name(:ratios_to_supplier_order_unit),
     ]
   end
 
   def data
-    @object.each do |o|
+    @object.each do |article|
       yield [
         '',
-        o.order_number,
-        o.name,
-        o.note,
-        o.manufacturer,
-        o.origin,
-        o.unit,
-        o.price,
-        o.tax,
-        o.deposit,
-        o.unit_quantity,
+        article.order_number,
+        article.name,
+        article.note,
+        article.manufacturer,
+        article.origin,
+        article.unit,
+        article.price,
+        article.tax,
+        article.deposit,
+        article.quantity,
+        article.supplier_order_unit,
+        article.price_unit,
+        article.group_order_unit,
+        article.group_order_granularity,
+        article.minimum_order_quantity,
         '',
         '',
-        o.article_category.try(:name),
+        article.article_category.try(:name),
+        article.article_unit_ratios.map { |ratio| "#{ratio.quantity} #{ratio.unit}" }.join(", ")
       ]
     end
   end
