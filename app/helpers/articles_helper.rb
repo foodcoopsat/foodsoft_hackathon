c68fbb49d825c71e5f9067ff5e2a763f01f03751
lentschi <office@florian-lentsch.at>
2024-07-12 19:00:05 +0200

    Fixes #66 (#74)
    * Fixes #66
    
    * Fixes broken integration tests
    
    * chore: refactor extract articles_helper article_version test setup function
    
    * chore: make helper functions private
    
    * On #66 Fixes https://github.com/foodcoopsat/foodsoft_hackathon/pull/74/files/b078cfaa87d334dba8eaafff5b2be152f293c448#r1670426999
    
    ---------
    
    Co-authored-by: Philipp Rothmann <philipprothmann@posteo.de>

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -15,13 +15,6 @@ module ArticlesHelper
     classes.join(' ')
   end
 
-  def format_unit(unit_property, article)
-    unit_code = article.send(unit_property)
-    return article.unit if unit_code.nil?
-
-    ArticleUnitsLib.human_readable_unit(unit_code)
-  end
-
   def format_supplier_order_unit(article)
     format_unit(:supplier_order_unit, article)
   end
@@ -32,23 +25,6 @@ module ArticlesHelper
 
   def format_billing_unit(article)
     format_unit(:billing_unit, article)
-  end
-
-  def format_unit_with_ratios(unit_property, article)
-    base = format_unit(unit_property, article)
-    unit_code = article.send(unit_property)
-    return base if ArticleUnitsLib.unit_is_si_convertible(unit_code)
-
-    relevant_units = [article.article_unit_ratios.map(&:unit)]
-    relevant_units << article.supplier_order_unit unless unit_property == :supplier_order_unit
-    first_si_convertible_unit = relevant_units
-                                .flatten
-                                .find { |unit| ArticleUnitsLib.unit_is_si_convertible(unit) }
-
-    return base if first_si_convertible_unit.nil?
-
-    quantity = article.convert_quantity(1, unit_code, first_si_convertible_unit)
-    "#{base} (#{number_with_precision(quantity, precision: 2, strip_insignificant_zeros: true)}\u00a0#{ArticleUnitsLib.units.to_h[first_si_convertible_unit][:symbol]})"
   end
 
   def format_supplier_order_unit_with_ratios(article)
@@ -76,5 +52,84 @@ module ArticlesHelper
       end
       safe_join(output)
     end
+  end
+
+  private
+
+  def format_unit_with_ratios(unit_property, article_version, reference_unit = :group_order_unit)
+    base = format_unit(unit_property, article_version)
+
+    factorized_unit_str = get_factorized_unit_str(article_version, unit_property, reference_unit) unless reference_unit.nil?
+    return base if factorized_unit_str.nil?
+
+    "#{base} (#{factorized_unit_str})"
+  end
+
+  def format_unit(unit_property, article)
+    unit_code = article.send(unit_property)
+    return article.unit if unit_code.nil?
+
+    ArticleUnitsLib.human_readable_unit(unit_code)
+  end
+
+  def get_factorized_unit_str(article_version, unit_property, reference_unit)
+    unit_code = article_version.send(unit_property)
+    reference_unit_code = article_version.send(reference_unit) || article_version.supplier_order_unit
+    return nil if ArticleUnitsLib.unit_is_si_convertible(unit_code)
+
+    factors = [{
+      quantity: article_version.convert_quantity(1, unit_code, reference_unit_code),
+      code: reference_unit_code
+    }]
+
+    first_si_conversible_unit_after_reference_unit = get_first_si_conversible_unit(article_version, reference_unit_code) unless ArticleUnitsLib.unit_is_si_convertible(reference_unit_code)
+    unless first_si_conversible_unit_after_reference_unit.nil?
+      factors << {
+        quantity: article_version.convert_quantity(1, reference_unit_code, first_si_conversible_unit_after_reference_unit),
+        code: first_si_conversible_unit_after_reference_unit
+      }
+    end
+
+    return nil if factors.length == 1 && factors.first[:quantity] == 1 && factors.first[:code] == unit_code
+
+    format_unit_factors(factors)
+  end
+
+  def get_first_si_conversible_unit(article_version, after_unit)
+    relevant_units = [article_version.supplier_order_unit] + article_version.article_unit_ratios.map(&:unit)
+
+    unit_index = relevant_units.find_index { |unit| unit == after_unit }
+    return nil if unit_index.nil?
+
+    relevant_units[unit_index + 1..].find { |unit| ArticleUnitsLib.unit_is_si_convertible(unit) }
+  end
+
+  def format_unit_factors(factors)
+    factor_str_arr = factors.each_with_index.map do |factor, index|
+      is_last = index + 1 == factors.length
+      format_unit_factor(factor, is_last)
+    end
+
+    factor_str_arr
+      .compact
+      .join("#{Prawn::Text::NBSP}×#{Prawn::Text::NBSP}")
+  end
+
+  def format_unit_factor(factor, with_unit)
+    return nil if !with_unit && factor[:quantity] == 1
+
+    quantity_str = number_with_precision(factor[:quantity], precision: 2, strip_insignificant_zeros: true)
+    return quantity_str unless with_unit
+
+    unit_data = ArticleUnitsLib.units.to_h[factor[:code]]
+    is_si_conversible = ArticleUnitsLib.unit_is_si_convertible(factor[:code])
+    unit_label = is_si_conversible ? unit_data[:symbol] : unit_data[:name]
+    return unit_label if factor[:quantity] == 1
+
+    multiplier_str = '×' unless is_si_conversible
+
+    [quantity_str, multiplier_str, unit_label]
+      .compact
+      .join(Prawn::Text::NBSP)
   end
 end
